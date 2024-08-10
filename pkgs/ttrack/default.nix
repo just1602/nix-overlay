1b5fe983be7e3cc8ddd8e3e706d6f5196b903373
Justin ! <just1602@riseup.net>
2024-08-10 17:09:03 -0400

    ttrack: bump to v0.1.0

--- pkgs/ttrack/default.nix
+++ pkgs/ttrack/default.nix
@@ -10,8 +10,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "just1602";
     repo = "ttrack";
-    rev = "210a7a2a09a9b49d227fc181fc7ee0358eb88551";
-    hash = "sha256-9BVZlh1SQ53yh0RgqmfYMlYoHu1IG09QAEDk55HInJs=";
+    rev = "v0.1.0";
+    hash = "sha256-gISMP/Ykvn1v8xf6mDpU3P++mduY/cpdoWsiU3aGIXA=";
   };
 
   cargoHash = "sha256-rs4XswUH5gV64iUmfZgtP0uCkA+5qFS3ZBrTVy5zqNc=";
