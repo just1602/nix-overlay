b0bf839afec742dac1b590b69e2fc31b9ac5976a
Justin ! <just1602@riseup.net>
2024-11-17 11:52:19 -0500

    todors: bump to 0.0.4

--- pkgs/todors/default.nix
+++ pkgs/todors/default.nix
@@ -6,16 +6,21 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "todors";
-  version = "0.0.2";
+  version = "0.0.4";
 
   src = fetchFromGitHub {
     owner = "just1602";
     repo = "todors";
     rev = "v${version}";
-    hash = "sha256-3mq25D/b3A3dCRLj/tlsFQ+5SDUhtY/T088R9sz9jms=";
+    hash = "sha256-KnqJTeZI6ms72oD1Z0278aHROnJgbNRDnUE9jmQf9So=";
   };
 
-  cargoHash = "sha256-ZDlf5jnsXxX+UiiGDIuW/v8+0lMUPWC8SVLJBsHG9MQ=";
+  # when debugging locally
+  # src = builtins.fetchGit {
+  #   url = "file:///home/goldman/dev/todors";
+  # };
+
+  cargoHash = "sha256-c7NZ//ttoRMqDz75MUnnZ64bcpD2stgoj8/v8xrhvEs=";
 
   meta = {
     description = "Todors";
