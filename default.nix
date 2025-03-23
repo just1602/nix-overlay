ee7254e86f35bf12d55367491d15512d30dc85f9
Justin ! <just1602@riseup.net>
2025-03-23 01:02:00 -0400

    Remove ttrack and todors since they now use flake

--- default.nix
+++ default.nix
@@ -1,5 +1,3 @@
 self: super: {
-  ttrack = super.callPackage ./pkgs/ttrack { };
-  todors = super.callPackage ./pkgs/todors { };
   puppet-editor-services = super.callPackage ./pkgs/puppet-editor-services { };
 }
